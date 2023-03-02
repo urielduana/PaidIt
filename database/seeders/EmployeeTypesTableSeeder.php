fe4e9f0e207edd108bc7142134d9eddaea957fce
Vadose <uri250602@outlook.es>
2023-03-01 23:25:01 -0600

    Seeder Format

--- database/seeders/EmployeeTypesTableSeeder.php
+++ database/seeders/EmployeeTypesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
 class EmployeeTypesTableSeeder extends Seeder
 {
     /**
