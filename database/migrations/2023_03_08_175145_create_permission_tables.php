c727149461a70372835e8196b3af41ffab68339d
urielduana <81336230+urielduana@users.noreply.github.com>
2023-03-08 13:50:08 -0600

    roles

--- database/migrations/2023_03_08_175145_create_permission_tables.php
+++ database/migrations/2023_03_08_175145_create_permission_tables.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\PermissionRegistrar;
-use Illuminate\Support\Facades\DB;
 
 class CreatePermissionTables extends Migration
 {
@@ -132,10 +131,11 @@ class CreatePermissionTables extends Migration
         if (empty($tableNames)) {
             throw new \Exception('Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
         }
-        
-        Schema::dropIfExists($tableNames['role_has_permissions']);
-        Schema::dropIfExists($tableNames['model_has_roles']);
-        Schema::dropIfExists($tableNames['model_has_permissions']);
-        Schema::dropIfExists($tableNames['permissions']);
+
+        Schema::drop($tableNames['role_has_permissions']);
+        Schema::drop($tableNames['model_has_roles']);
+        Schema::drop($tableNames['model_has_permissions']);
+        Schema::drop($tableNames['roles']);
+        Schema::drop($tableNames['permissions']);
     }
 }
