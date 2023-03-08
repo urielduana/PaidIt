36c77c75dcf4cc825dfb5640b134ae53c4702707
urielduana <81336230+urielduana@users.noreply.github.com>
2023-03-07 19:28:01 -0600

    roles migrations

--- database/migrations/01_2014_10_12_000000_create_users_table.php
+++ database/migrations/01_2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('role')->nullable();
             $table->integer('age');
             $table->bigInteger('phone');
             $table->string('email')->unique();
