59abbef83dd8549d649f3688c5aecf70cd864cf6
urielduana <uri250602@outlook.es>
2023-03-26 01:54:01 -0600

    Mejorando tabla

--- resources/js/Pages/Customer/Item/Index.vue
+++ resources/js/Pages/Customer/Item/Index.vue
@@ -2,24 +2,24 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { useDark, useToggle } from "@vueuse/core";
 import { reactive, watchEffect } from "vue";
+import { groupBy } from "lodash";
+import { onMounted } from "vue";
+
+// Dark Mode
 const isDark = useDark();
-
 const state = reactive({
-  darkMode: useDark().value,
-  tableMode: "",
-})
+    darkMode: useDark().value,
+    tableMode: "",
+});
 watchEffect(() => {
-  state.darkMode = useDark().value
-  if (state.darkMode) {
-    state.tableMode = "nocturnal";
-  } else {
-    state.tableMode = "polar-bear";
-  }
-})
-
-
-
-
+    state.darkMode = useDark().value;
+    if (state.darkMode) {
+        state.tableMode = "nocturnal";
+    } else {
+        state.tableMode = "polar-bear";
+    }
+});
+// Props
 const props = defineProps({
     items: {
         type: Object,
@@ -27,6 +27,7 @@ const props = defineProps({
     },
 });
 
+// Table
 const columns = [
     {
         label: "",
@@ -61,9 +62,12 @@ const columns = [
         width: "30%",
     },
 ];
-
-const rows = props.items.map((item) => {
-    return {
+const rows = Object.values(
+    groupBy(props.items, (item) => item.item__business.name)
+).map((group) => ({
+    mode: "span",
+    label: group[0].item__business.name,
+    children: group.map((item) => ({
         id: item.id,
         image: item.image,
         name: item.name,
@@ -71,35 +75,36 @@ const rows = props.items.map((item) => {
         price: item.price,
         stock: item.stock,
         category: item.item__item__type.name,
-    };
-});
-
+    })),
+}));
+console.log(rows);
 const paginationOptions = {
-    enabled: true,
+    enabled: false,
     perPage: 20,
     perPageDropdownEnabled: true,
 };
 const searchOptions = {
     enabled: true,
     skipDiacritics: true,
-    placeholder: 'Search...',
+    placeholder: "Search...",
 };
-
-console.log(rows);
+const groupOptions = {
+    enabled: true,
+    collapsable: true,
+};
 </script>
 
 <template>
     <AppLayout title="Customer">
         <template #header>
-            <h2>
-                Available Products
-            </h2>
+            <h2>Available Products</h2>
         </template>
         <div class="py-5">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-xl sm:rounded-lg">
                     <div>
                         <vue-good-table
+                            ref="myCustomTable"
                             :columns="columns"
                             :rows="rows"
                             max-height="500px"
@@ -107,8 +112,19 @@ console.log(rows);
                             :theme="state.tableMode"
                             :pagination-options="paginationOptions"
                             :search-options="searchOptions"
+                            :group-options="groupOptions"
+                            styleClass="vgt-table striped"
                         >
                             <template #table-row="props">
+                                <span
+                                    v-if="props.column.field == 'image'"
+                                    class="flex justify-center"
+                                >
+                                    <img
+                                        :src="props.row.image"
+                                        class="h-10 w-10"
+                                    />
+                                </span>
                                 <span v-if="props.column.field == 'price'">
                                     <span>${{ props.row.price }}</span>
                                 </span>
@@ -120,3 +136,12 @@ console.log(rows);
         </div>
     </AppLayout>
 </template>
+
+<script>
+export default {
+    mounted() {
+        this.$refs.myCustomTable.expandAll();
+    },
+    // ...
+};
+</script>
