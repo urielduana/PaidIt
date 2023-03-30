0a3c1b03fdc04f7128f177e596f8caaf5722c24e
urielduana <uri250602@outlook.es>
2023-03-30 00:28:39 -0600

    a

--- resources/js/Pages/Employee/Employee.vue
+++ resources/js/Pages/Employee/Employee.vue
@@ -1,33 +1,42 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
 
-
-// const props = defineProps({
-//     // role: {
-//     //     type: String,
-//     //     required: true
-//     // },
-//     // example: Object
-// });
-// console.log(props);
-
-  
+const props = defineProps({
+    user: {
+        type: Object,
+        required: true,
+    },
+    employee: {
+        type: Object,
+        required: true,
+    },
+    business: {
+        type: Object,
+        required: true,
+    },
+    roles: {
+        type: Array,
+        required: true,
+    },
+    permissions: {
+        type: Array,
+        required: true,
+    },
+});
 </script>
 
 <template>
     <AppLayout title="Employee">
         <template #header>
-            <h2>
-                Employee
-            </h2>
+            <h2>Employee</h2>
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-xl sm:rounded-lg flex justify-center">
                     <!-- Iniciar Tarjeta Info-Usuario-->
                     <div
-                        class="mb-20 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                        class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
                     >
                         <div class="flex flex-col items-center py-10 px-4">
                             <img
@@ -45,7 +54,31 @@ import Welcome from '@/Components/Welcome.vue';
                                 >Empleado</span
                             >
 
-                    
+                            <span
+                                class="text-sm text-center my-4 text-gray-500 dark:text-gray-400"
+                                ><span class="font-bold text-white"
+                                    >Negocio:</span
+                                >
+                                {{ business.name }}
+                            </span>
+
+                            <span
+                                class="text-sm text-center mb-2 text-gray-500 dark:text-gray-400"
+                                ><span class="font-bold text-white"
+                                    >Roles:</span
+                                >
+                                {{ roles.name }}
+                            </span>
+                            <span
+                                class="text-sm text-center mb-2 text-gray-500 dark:text-gray-400"
+                                ><span class="font-bold text-white"
+                                    >Permisos:</span
+                                >
+                                {{ permissions.name }}
+                            </span>
+                        </div>
+                    </div>
+                    <!-- Finaliza tarjeta Info-Usuario-->
                 </div>
             </div>
         </div>
