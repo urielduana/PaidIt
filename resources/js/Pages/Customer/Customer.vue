c2ba38499840ac48674525799d58353db0a0dcc0
Angel Montoya <j.gustavo.m245@gmail.com>
2023-07-26 15:27:45 -0600

    Add functionality - Grafica ingresos y gastos

--- resources/js/Pages/Customer/Customer.vue
+++ resources/js/Pages/Customer/Customer.vue
@@ -12,160 +12,107 @@ const props = defineProps({
         required: true,
     },
 });
+// Datos de ejemplo de transacciones
+const transactions = [
+    { amount: 1000, type: 'income' },
+    { amount: 500, type: 'expense' },
+    { amount: 2000, type: 'income' },
+    { amount: 500, type: 'expense' },
+    // ... más transacciones
+];
+
+// Función para normalizar los valores dentro de un rango específico
+function normalizeValue(value, min, max) {
+    return (value - min) / (max - min);
+}
+
+// Calculamos los totales de ingresos y gastos
+const totalIncome = transactions.filter(transaction => transaction.type === 'income')
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+const totalExpenses = transactions.filter(transaction => transaction.type === 'expense')
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+// Encontramos el valor máximo de ingresos y gastos para normalizar los valores
+const maxIncome = Math.max(totalIncome, totalExpenses);
+const maxExpenses = maxIncome;
+
+// Calculamos los porcentajes normalizados de ingresos y gastos
+const incomePercentage = normalizeValue(totalIncome, 0, maxIncome) * 100;
+const expensePercentage = normalizeValue(totalExpenses, 0, maxExpenses) * 100;
 </script>
 
 <template>
-    <AppLayout title="Customer">
-        <template #header>
-            <h2 class="font-semibold text-xlleading-tight text-xl">Customer</h2>
-        </template>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center m-12">
-                    <!-- Iniciar Tarjeta Info-Usuario-->
-                    <div
-                        class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                    >
-                        <div class="flex flex-col items-center py-10 px-4">
-                            <img
-                                class="w-24 h-24 mb-3 rounded-full shadow-lg"
-                                :src="user.profile_photo_path"
-                                alt="user image"
-                            />
-                            <h5
-                                class="mb-1 text-xl text-center font-medium text-gray-900 dark:text-white"
-                            >
-                                {{ user.name }}
-                            </h5>
-                            <span
-                                class="text-sm text-center text-gray-500 dark:text-gray-400"
-                                >Cliente</span
-                            >
+  <AppLayout title="Customer">
+      <template #header>
+          <h2 class="font-semibold text-xl leading-tight text-xl">Customer</h2>
+      </template>
+      <div class="py-12">
+          <div class="flex justify-around py-6">
+              <!-- Iniciar Tarjeta Info-Usuario-->
+              <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-[#1C2532]">
+                  <div class="flex flex-col items-center py-10 px-4">
+                      <img
+                          class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                          :src="user.profile_photo_path"
+                          alt="user image"
+                      />
 
-                            <h2
-                                class="text-sm text-center my-4 font-bold flex gap-2"
-                            >
-                                Titular:
-                                <h2 class="font-normal">
-                                    {{ customer.guardian_name }}
-                                </h2>
-                            </h2>
-                        </div>
-                    </div>
-                    <!-- Finaliza tarjeta Info-Usuario-->
+                      <!-- Ingreso -->
+                      <va-progress-bar
+                          :model-value="incomePercentage"
+                          size="large"
+                          content-inside
+                          color="primary"
+                      >
+                          Income
+                      </va-progress-bar>
 
-                    <!-- Inicia saldo -->
-                    <div
-                        class="flex items-center w-96 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                    >
-                        <div class="w-full text-center">
-                            <h6
-                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                            >
-                                Cuenta
-                            </h6>
-                            <p
-                                class="font-normal my-4 text-gray-700 dark:text-gray-400"
-                            >
-                                Saldo disponible:
-                            </p>
-                            <h2 class="my-4 text-center font-bold">
-                                $ {{ customer.balance }}
-                            </h2>
-                            <div class="flex justify-center">
-                                <button
-                                    @click="showModal = true"
-                                    type="button"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                                >
-                                    Depositar
-                                </button>
-                                <DialogModal
-                                    :show="showModal"
-                                    @close="showModal = false"
-                                    :max-width="modalMaxWidth"
-                                    :closeable="modalCloseable"
-                                >
-                                    <template #title> Depositar </template>
-                                    <template #content>
-                                        <div>
-                                            <label
-                                                for="cantidad"
-                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                                >Cantidad</label
-                                            >
-                                            <input
-                                                type="number"
-                                                name="cantidad"
-                                                id="cantidad"
-                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                                placeholder="00.00"
-                                                required
-                                                v-model="formData.cantidad"
-                                            />
-                                        </div>
-                                    </template>
-                                    <template #footer>
-                                        <button
-                                            @click="submitForm()"
-                                            type="submit"
-                                            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                        >
-                                            Enviar
-                                        </button>
-                                    </template>
-                                </DialogModal>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Finaliza saldo -->
-                </div>
-            </div>
-        </div>
-    </AppLayout>
+                      <!-- Egresos -->
+                      <va-progress-bar
+                          :model-value="expensePercentage"
+                          size="large"
+                          content-inside
+                          color="danger"
+                      >
+                          Bills
+                      </va-progress-bar>
+
+                      <h5 class="mb-1 text-xl text-center font-extrabold text-gray-900 dark:text-white">
+                          <!--{{ user.name }}-->
+                          $1,500.00
+                      </h5>
+                      <span class="text-sm text-center text-gray-500 dark:text-gray-400">Total Revenue</span>
+                      <div>
+                          <h2 class="text-sm text-centerfont-bold">
+                              {{ user.name }}
+                          </h2>
+                          <span class="font-normal">customer</span>
+                      </div>
+                      <div>
+                          <h2>
+                              {{ customer.guardian_name }}
+                          </h2>
+                          <span>Tutor</span>
+                      </div>
+                  </div>
+              </div>
+              <!-- Finaliza tarjeta Info-Usuario-->
+
+              <!-- Inicia saldo -->
+              <div class="flex items-center w-96 max-w-2xl bg-white border border-gray-200 rounded-lg shadow dark:bg-[#1C2532] dark:border-gray-700">
+                  <div class="w-full text-center">
+                      <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Cuenta
+                      </h6>
+                      <p class="font-normal my-4 text-gray-700 dark:text-gray-400">Saldo disponible:</p>
+                      <h2 class="my-4 text-center font-bold">
+                          $ {{ customer.balance }}
+                      </h2>
+                  </div>
+              </div>
+              <!-- Finaliza saldo -->
+          </div>
+      </div>
+  </AppLayout>
 </template>
-
-<script>
-import Modal from "../../Components/Modal.vue";
-
-export default {
-    components: {
-        Modal,
-        DialogModal,
-    },
-
-    data() {
-        return {
-            showModal: false,
-            modalMaxWidth: "2xl",
-            modalCloseable: true,
-            formData: {
-                cantidad: null,
-            },
-        };
-    },
-
-    methods: {
-        submitForm() {
-            // Envía los datos al backend utilizando Inertia
-            this.$inertia
-                .post("/customer", this.formData)
-                .then(() => {
-                    // Actualiza los datos en el frontend
-                    this.formData.cantidad = null;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-    },
-};
-</script>
-
-<style>
-input[type="number"]::-webkit-inner-spin-button,
-input[type="number"]::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-}
-</style>
