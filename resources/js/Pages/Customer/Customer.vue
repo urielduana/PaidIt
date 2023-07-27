1b8fd68fe3fefe66893b3ba3cd3821489b4f101e
Angel Montoya <j.gustavo.m245@gmail.com>
2023-07-26 20:59:23 -0600

    add style table activity

--- resources/js/Pages/Customer/Customer.vue
+++ resources/js/Pages/Customer/Customer.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import DialogModal from "@/Components/DialogModal.vue";
 
 const props = defineProps({
     user: {
@@ -46,6 +45,150 @@ const expensePercentage = normalizeValue(totalExpenses, 0, maxExpenses) * 100;
 const totalRevenue = totalIncome - totalExpenses;
 const img =
     "https://img.freepik.com/vector-gratis/fondo-galaxia-realista_52683-12122.jpg?q=10&h=200";
+
+const items = [
+    {
+        id: 1,
+        created_at: "2023-07-26",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 2,
+        created_at: "2023-07-25",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 3,
+        created_at: "2023-07-24",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 4,
+        created_at: "2023-07-23",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 5,
+        created_at: "2023-07-22",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 6,
+        created_at: "2023-07-21",
+        status: {
+            status: "expense",
+        },
+    },
+    // Elementos adicionales
+    {
+        id: 7,
+        created_at: "2023-07-20",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 8,
+        created_at: "2023-07-19",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 9,
+        created_at: "2023-07-18",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 10,
+        created_at: "2023-07-17",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 11,
+        created_at: "2023-07-16",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 12,
+        created_at: "2023-07-15",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 13,
+        created_at: "2023-07-14",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 14,
+        created_at: "2023-07-13",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 15,
+        created_at: "2023-07-12",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 16,
+        created_at: "2023-07-11",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 17,
+        created_at: "2023-07-10",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 18,
+        created_at: "2023-07-09",
+        status: {
+            status: "expense",
+        },
+    },
+    {
+        id: 19,
+        created_at: "2023-07-08",
+        status: {
+            status: "income",
+        },
+    },
+    {
+        id: 20,
+        created_at: "2023-07-07",
+        status: {
+            status: "expense",
+        },
+    },
+];
 </script>
 
 <template>
@@ -131,22 +274,56 @@ const img =
 
                 <!-- Inicia saldo -->
                 <div
-                    class="flex items-center w-96 max-w-2xl bg-white border border-gray-200 rounded-lg shadow dark:bg-[#1C2532] dark:border-gray-700"
+                    class="max-w-3xl bg-white border border-gray-200 rounded-lg shadow dark:bg-[#1C2532] dark:border-gray-700 p-4 grid content-center gap-10"
                 >
-                    <div class="w-full text-center">
-                        <h6
-                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                        >
-                            Cuenta
-                        </h6>
-                        <p
-                            class="font-normal my-4 text-gray-700 dark:text-gray-400"
-                        >
-                            Saldo disponible:
-                        </p>
-                        <h2 class="my-4 text-center font-bold">
-                            $ {{ customer.balance }}
-                        </h2>
+                    <h6
+                        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white justify-items-center grid"
+                    >
+                        Activity
+                    </h6>
+                    <div class="w-full text-center table-container">
+                        <table class="table-auto w-full">
+                            <!-- Encabezado de la tabla -->
+                            <thead
+                                class="sticky top-0 bg-white dark:bg-[#1C2532]"
+                            >
+                                <tr>
+                                    <th class="px-4 py-2">ID</th>
+                                    <th class="px-4 py-2">Tipo</th>
+                                    <th class="px-4 py-2">Fecha</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="item in items"
+                                    :key="item.id"
+                                    class="table-row-divider"
+                                >
+                                    <td class="border px-4 py-2">
+                                        {{ item.id }}
+                                    </td>
+                                    <td class="border px-4 py-2">
+                                        <div
+                                            :class="{
+                                                'text-green-500':
+                                                    item.status.status ===
+                                                    'income',
+                                                'text-red-500':
+                                                    item.status.status ===
+                                                    'expense',
+                                            }"
+                                            class="py-1 px-2 rounded-md font-semibold"
+                                        >
+                                            {{ item.status.status }}
+                                        </div>
+                                    </td>
+
+                                    <td class="border px-4 py-2">
+                                        {{ item.created_at }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <!-- Finaliza saldo -->
@@ -154,3 +331,24 @@ const img =
         </div>
     </AppLayout>
 </template>
+
+<style>
+.table-container {
+    max-height: 250px;
+    overflow-y: auto;
+}
+thead.sticky {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+}
+.table-container::-webkit-scrollbar {
+    width: 8px;
+    background-color: none; 
+}
+
+.table-container::-webkit-scrollbar-thumb {
+    background-color: #223a59; 
+    border-radius: 4px; 
+}
+</style>
