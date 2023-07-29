4025e0551daf784c7288be4d2c13fed52815bcf1
Tu nombre <tucorreo@example.com>
2023-07-29 13:32:02 -0600

    Vistas de errores, falta ruta para mostrarlas

---
+++ resources/js/Pages/PageforErrors/Error403.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+defineProps({
+    imagenURL: String,
+    texto: String   
+});
+</script>
+
+
+<template>
+    <div class="error_403">
+      <img src="public/img/accesodenegado.png" alt="" />
+      <div class="texto">
+        Error!
+        Parece que no tienes acceso
+        a esta pagina.
+        <p>{{ texto }}</p>
+      </div>
+    </div>
+  </template>
+  
+  
+  <style>
+  .error_404 {
+    display: flex;
+    align-items: center;
+  }
+  
+  .texto {
+    /* Estilos para el contenedor del texto */
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: right;
+    color: blueviolet;
+  }
+  
+  /* Estilos adicionales para la imagen (opcional) */
+  img {
+    max-width: 100%;
+  }
+  </style>
+  
---
+++ resources/js/Pages/PageforErrors/Error404.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+defineProps({
+    imagenURL: String,
+    texto: String   
+});
+</script>
+
+
+<template>
+    <div class="error_404">
+      <img src="/public/img/error404.png" alt="" />
+      <div class="texto">
+        Parece que no pudimos encontrar 
+        lo que estabas buscando.
+        Sera mejor que visites otra
+        de nuestras paginas.
+        <p>{{ texto }}</p>
+      </div>
+    </div>
+  </template>
+  
+  
+  <style>
+  .error_404 {
+    display: flex;
+    align-items: center;
+  }
+  
+  .texto {
+    /* Estilos para el contenedor del texto */
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: right;
+    color: blueviolet;
+  }
+  
+  /* Estilos adicionales para la imagen (opcional) */
+  img {
+    max-width: 100%;
+  }
+  </style>
+  
