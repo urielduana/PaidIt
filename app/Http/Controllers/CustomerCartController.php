69e3b5ba52afb9f24d4a53434a78a9102e9a4014
Twzty <119033777@upq.edu.mx>
2023-08-02 01:22:41 -0600

    implementacion de logica para cus y bus Transactio

--- app/Http/Controllers/CustomerCartController.php
+++ app/Http/Controllers/CustomerCartController.php
@@ -8,6 +8,9 @@ use App\Models\Cart;
 use App\Models\CartItem;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\BusinessesTransaction;
+use App\Models\CustomersTransaction;
+use App\Models\Item;
 class CustomerCartController extends Controller
 {
     /**
@@ -17,109 +20,17 @@ class CustomerCartController extends Controller
     {
         $authenticated = auth()->user();
         $customer = $authenticated->user_Customer;
-
-        // Accede a las órdenes relacionadas con el usuario autenticado
-        $carts = Cart::where('customer_id', $customer->id)->get();
-        $cartItems = CartItem::whereIn('cart_id', $carts->pluck('id'))->get();
-
-        // Iterar sobre los carritos y asignar los Cart_Item correspondientes
-        foreach ($carts as $cart) {
-            $cart->cartItems= collect();
-            foreach ($cartItems as $item) {
-                if ($cart->id === $item->Cart_id  ) {
-
-                    $cart->cartItems->push($item);
-                }
-            }
-        }
-        
+        $cart = $customer->customer_Cart;
+        $cartItems = $cart ? $cart->cart_CartItem : [];
+        $order= $customer->customer_Order;
+        $orderItems= $order ? $order[0]->order_Order_Item : [];
         return Inertia::render('Customer/Cart/CustomerCart',[
             'user' => $customer,
-            'carts' => $carts,
+            'cart' => $cart,
+            'cartItems'=>$cartItems,
+            'orderItems'=>$orderItems,
         ]);
     }
-    
-public function addToCart(Request $request)
-{
-    $productId = $request->input('product_id');
-    $quantity = $request->input('quantity', 1); // Cantidad predeterminada es 1
-
-    $authenticated = auth()->user();
-    $customer = $authenticated->user_Customer;
-
-    // Verificar si ya existe un carrito para el cliente
-    $cart = $customer->cart_Customer;
-
-    if (!$cart) {
-        // Si no hay un carrito para el cliente, crear uno nuevo
-        $cart = new Cart(['customer_id' => $customer->id]);
-        $cart->save();
-    }
-
-    // Verificar si el item ya está en el carrito
-    $existingCartItem = $cart->cart_Cart_Item()->where('item_id', $productId)->first();
-
-    if ($existingCartItem) {
-        // Si el item ya está en el carrito, actualizamos la cantidad
-        $existingCartItem->quantity += $quantity;
-        $existingCartItem->save();
-    } else {
-        // Si el item no está en el carrito, lo agregamos
-        $item = Item::findOrFail($productId);
-        $cartItem = new CartItem([
-            'quantity' => $quantity,
-            'item_id' => $item->id,
-        ]);
-
-        $cart->cart_Cart_Item()->save($cartItem);
-    }
-
-    // Redireccionar de vuelta a la página del carrito con un mensaje de éxito
-    return to_route('carts.index')->with('success', 'Item agregado al carrito.');
-}
-    public function completePurchase(Request $request)
-    {
-        $authenticated = auth()->user();
-        $customer = $authenticated->user_Customer;
-    
-        // Buscar el carrito activo del cliente actual
-        $cart = Cart::where('customer_id', $customer->id)
-                    ->first();
-    
-        if (!$cart) {
-            // Si no hay un carrito activo, redireccionar con un mensaje de error
-            return redirect()->route('carts.index')->with('error', 'No se encontró ningún carrito activo.');
-        }
-    
-        // Crear una nueva orden y establecer el estado como "completado"
-        $order = Order::create([
-            'total' => $cart->total,
-            'status' => 'completed', // Establecer el estado de la Orden como 'completada'
-            'description' => 'Compra realizada', // Puedes agregar una descripción de la orden
-            'customer_id' => $customer->id,
-        ]);
-    
-        // Mover los items del carrito a la tabla OrderItems y relacionarlos con la nueva Orden
-        foreach ($cart->cart_Cart_Item as $cartItem) {
-            $orderItem = new OrderItem([
-                'quantity' => $cartItem->quantity,
-                'price' => $cartItem->item->price,
-                'total' => $cartItem->quantity * $cartItem->item->price,
-                'status' => 'completed', // Puedes establecer el estado del OrderItem como 'completado'
-                'order_id' => $order->id, // Relacionar el OrderItem con la nueva Orden
-                'item_id' => $cartItem->item->id,
-            ]);
-    
-            $orderItem->save();
-        }
-    
-        // Eliminar el carrito ahora que la compra se ha completado
-        $cart->delete();
-    
-        // Redireccionar a la página de órdenes del cliente con un mensaje de éxito
-        return to_route('orders.index')->with('success', 'Compra completada.');
-    }
-    
     /**
      * Show the form for creating a new resource.
      */
@@ -133,7 +44,39 @@ public function addToCart(Request $request)
      */
     public function store(Request $request)
     {
-        //
+        $authenticated = auth()->user();
+        $customer = $authenticated->user_Customer;
+        $cart = $customer->customer_Cart;
+        $cartItems= $cart ->cart_CartItem;
+        if (!($cart)) {
+            // Si no hay un carrito activo, redireccionar con un mensaje de error
+            return Inertia::location(url())->with('error', 'No se encontró ningún carrito activo.');
+        }else{
+
+        // Crear una nueva orden y establecer el estado como "completado"
+        $order = Order::create([
+            'description' => 'Compra realizada', // Puedes agregar una descripción de la orden
+            'Customer_id' => $cart->Customer_id,
+        ]);
+        // Mover los items del carrito a la tabla OrderItems y relacionarlos con la nueva Orden
+        foreach ($cartItems as $item) {
+            $orderItem = OrderItem::create([
+                'quantity' => $item->quantity,
+                'Order_id' => $order->id, // Relacionar el OrderItem con la nueva Orden
+                'Item_id' => $item->Item_id,
+                'Status_id' => 1,
+            ]);
+        }
+        $this->createCustomerTransactions($order);
+        $this->createBusinessesTransactions($order);
+        // Eliminar el carrito ahora que la compra se ha completado
+        $cart->delete();
+    
+        // Redireccionar a la página de órdenes del cliente con un mensaje de éxito
+        return Inertia::location(url()->previous());
+        
+        }
+
     }
 
     /**
@@ -157,14 +100,59 @@ public function addToCart(Request $request)
      */
     public function update(Request $request, string $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
-    {
-        //
+    {   
+        
     }
+    public function createCustomerTransactions($order){
+        $orderItems= $order->order_Order_Item;
+        $items=[];
+        $total=0;
+        foreach ($orderItems as $key => $orderItem) {
+            $items[$key]=$orderItem->orderItem_Item;
+            $total+=$items[$key]->price*$orderItem->quantity;
+        }
+        $transaction = CustomersTransaction::create([
+            'name' => 'Compra realizada',
+            'mount' => $total,
+            'customer_id' => $order->Customer_id,
+        ]);
+    }
+
+    public function createBusinessesTransactions($order) {
+        // Obtener los order items del objeto $order
+        $orderItems = $order->order_Order_Item;
+    
+        // Inicializar el array para almacenar los totales por negocio
+        $businessTotals = [];
+    
+        // Calcular la sumatoria del precio por item y cantidad para cada negocio
+        foreach ($orderItems as $orderItem) {
+            $item = $orderItem->orderItem_Item;
+            $businessId = $item->Business_id;
+    
+            // Verificar si ya existe un total para el Business_id actual
+            if (!isset($businessTotals[$businessId])) {
+                // Si no existe, inicializar el total para este negocio con el precio del primer item encontrado
+                $businessTotals[$businessId] = $item->price * $orderItem->quantity;
+            } else {
+                // Si ya existe, simplemente sumar el precio del item actual multiplicado por su cantidad al total existente
+                $businessTotals[$businessId] += $item->price * $orderItem->quantity;
+            }
+        }
+
+        foreach ($businessTotals as $business_id=>$total) {
+            $transaction = BusinessesTransaction::create([
+                'name' => 'Venta realizada',
+                'mount' => $total,
+                'business_id' => $business_id,
+            ]);
+        }
+    }
+    
 }
