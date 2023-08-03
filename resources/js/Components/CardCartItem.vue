b15f643eca1fc5dbd16cbda70d4e75529df8c9f8
Twzty <119033777@upq.edu.mx>
2023-08-03 01:21:54 -0600

    avance en componente de cartItems

---
+++ resources/js/Components/CardCartItem.vue
@@ -0,0 +1,86 @@
+<script setup>
+import SectionBorder from "./SectionBorder.vue";
+import { useDark, useToggle } from "@vueuse/core";
+import { reactive, watchEffect, defineProps, ref } from "vue";
+
+// Dark Mode
+const isDark = useDark();
+const state = reactive({
+    darkMode: useDark().value,
+    tableMode: "",
+});
+watchEffect(() => {
+    state.darkMode = useDark().value;
+    if (state.darkMode) {
+        state.tableMode = "nocturnal";
+    } else {
+        state.tableMode = "polar-bear";
+    }
+});
+defineProps({
+    title: {
+        default: "Titulo de ejemplo, no haz enviado un titulo",
+        type: String,
+        required: true,
+    },
+    image: {
+        default: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        type: String,
+        required: false,
+    },
+    business: {
+        default: "Business:Business 1",
+        type: String,
+        required: false,
+    },
+    quantity: {
+        type: String,
+        required: false,
+    },
+    price: {
+        type: String,
+        required: false,
+    },
+});
+</script>
+<template>
+    <div class="m-4 relative max-w-xl bg-red-600">
+        <!-- Div que contiene la imagen del item -->
+        <div class="absolute w-1/5 top-1/4 bottom-1/4 ml-5">
+            <img
+                class="rounded-xl object-cover w-20 h-full md:w-32 lg:w-40 xl:w-48"
+                src="https://flowbite.com/docs/images/blog/image-1.jpg"
+                alt=""
+            />
+        </div>
+        <!-- Div que contiene la información extra del item -->
+        <div
+            class="p-3 md:p-4 ml-10 md:ml-28 rounded-l-3xl rounded-r-lg bg-white border border-gray-200 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 flex items-center"
+        >
+            <!-- Puedes utilizar clases de Tailwind CSS para establecer los estilos del borde y otros elementos -->
+            <SectionBorder :padding="py - 2" />
+            <div
+                class="overflow-hidden flex flex-col flex-grow mb-10 ml-4 font-normal font-paidit-500"
+            >
+                <p class="text-gray-600">{{ business }}</p>
+                <p class="text-gray-700 dark:text-gray-50">{{ title }}</p>
+                <p class="text-gray-600">{{ price }}</p>
+            </div>
+            <div
+                class="flex flex-col flex-grow ml-4 sm:ml-6 md:ml-8 lg:ml-10 xl:ml-12 mb-3 font-normal text-gray-700 dark:text-gray-400"
+            >
+                <p class="mb-2">Párr 1</p>
+                <p class="mb-2">Párrafo 2</p>
+                <p>Párrafo 3</p>
+            </div>
+            <div
+                class="flex flex-col flex-grow ml-4 sm:ml-6 md:ml-8 lg:ml-10 xl:ml-12 mb-3 font-normal text-gray-700 dark:text-gray-400"
+            >
+                <p class="mb-2">Párrafo 1</p>
+                <p class="mb-2">Párrafo 2</p>
+                <p>Párrafo 3</p>
+            </div>
+            <!-- Aquí puedes agregar más contenido de la información extra si es necesario -->
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Customer/Cart/CustomerCart.vue
+++ resources/js/Pages/Customer/Cart/CustomerCart.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { router } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import CardCartItem from "@/Components/CardCartItem.vue";
 
 const props = defineProps({
     user: {
@@ -19,20 +21,48 @@ const props = defineProps({
         type: Object,
         required: false,
     },
+    items: {
+        type: Object,
+        required: false,
+    },
 });
 const completeOrder = async () => router.post("carts");
 </script>
-
 <template>
-    <div>
-        <!-- Mostrar detalles del carrito y sus items aquí -->
-        <h1>{{ cartItems }}</h1>
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>{{ orderItems }}</h1>
-        <!-- Botón para completar la orden -->
-        <button @click="completeOrder">Completar Orden</button>
-    </div>
+    <AppLayout title="Customer">
+        <template #header>
+            <h2>Shopping Carts</h2>
+        </template>
+
+        <div class="flex flex-row">
+            <div class="basis-1/2">
+                <va-scroll-container vertical class="w-1/2 max-h-[95%]">
+                    <div v-for="item in items" :key="item.id">
+                        <div class="overflow-hidden flex flex-row-reverse">
+                            <CardCartItem
+                                :title="item.name"
+                                :business="item.business_id"
+                                :price="item.price"
+                            ></CardCartItem>
+                            <!-- los puntos antes de title es para indicar la variable -->
+                        </div>
+                    </div>
+                </va-scroll-container>
+            </div>
+            <div class="basis-1/2">
+                <va-scroll-container vertical rtl class="w-1/2 max-h-[95%]">
+                    <div>
+                        <CardCartItem title="holaaaaaaaaaaaaaaa">
+                        </CardCartItem>
+                    </div>
+                </va-scroll-container>
+            </div>
+        </div>
+
+        <div class="flex gap-4"></div>
+        <div>
+            <!-- Botón para completar la orden -->
+            <button @click="completeOrder">Completar Orden</button>
+        </div>
+    </AppLayout>
 </template>
