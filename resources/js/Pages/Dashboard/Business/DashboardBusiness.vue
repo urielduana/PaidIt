7d4b003125295de5a50c1be768bea18539d7d267
Copado17 <120035491@upq.edu.mx>
2023-08-04 15:03:18 -0600

    Por si las moscas

--- resources/js/Pages/Dashboard/Business/DashboardBusiness.vue
+++ resources/js/Pages/Dashboard/Business/DashboardBusiness.vue
@@ -1,3 +1,17 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+
+
 <template>
-    
+  <AppLayout title="Customer">
+        <template #header>
+            <h2>Si funciono wey</h2>
+        </template>
+        <div class="py-5">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <!-- Contenido -->
+            </div>
+        </div>
+    </AppLayout>  
 </template>
