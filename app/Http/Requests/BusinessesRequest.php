9ef75c62e2b1dec1b51d51b95cb5371db89d0718
Copado17 <120035491@upq.edu.mx>
2023-08-11 15:23:10 -0600

    Created Business

---
+++ app/Http/Requests/BusinessesRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BusinessesRequest extends FormRequest
+
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'phone' => 'required|numeric | digits:10',
+            'address' => 'required|string|max:255',
+        ];
+    }
+    
+}
